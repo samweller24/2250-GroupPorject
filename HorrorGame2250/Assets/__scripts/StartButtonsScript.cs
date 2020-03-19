ecf6ef965a6d4ea2c037d006699ecbcbd55e83c8
Mitchell Zehr <zehrmitch@gmail.com>
2020-03-19 16:09:45 -0400

    MainLogoAndQuit
    Added the new main logo and some small graphical updates

--- HorrorGame2250/Assets/__scripts/StartButtonsScript.cs
+++ HorrorGame2250/Assets/__scripts/StartButtonsScript.cs
@@ -9,4 +9,9 @@ public class StartButtonsScript : MonoBehaviour
     {
         SceneManager.LoadScene("playerSelect");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
